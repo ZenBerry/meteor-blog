e8762887e140a20da0943e3fa5e4610e3443a9ab
Eugene ZenBerry <eugeneberezhnoy@gmail.com>
2023-02-22 08:09:39 +0200

    Working on auth

--- server/main.js
+++ server/main.js
@@ -1,5 +1,14 @@
 import { Meteor } from 'meteor/meteor';
+import { ServiceConfiguration } from 'meteor/service-configuration';
 
 Meteor.startup(() => {
   student = new Mongo.Collection('student');
+  ServiceConfiguration.configurations.upsert({
+    service: "google"
+  }, {
+    $set: {
+      clientId: "<your client ID>",
+      secret: "<your client secret>"
+    }
+  });
 });
