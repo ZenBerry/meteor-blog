969731d236b5da9c5f580e19361a0b18f2a2d5ad
Eugene ZenBerry <eugeneberezhnoy@gmail.com>
2023-02-22 15:20:04 +0200

    Edit posts

--- client/main.js
+++ client/main.js
@@ -1,5 +1,3 @@
-var posts;
-
 import './main.html';
 
 import {
@@ -10,18 +8,27 @@ posts = new Mongo.Collection('posts');
 
 Template.Insert.events({
   'click .post-button': function(event) {
-    var d_post;
-    
-    //Taking the Value in Registers
+    let d_post;
+    let postId = Session.get('editingPostId'); // get the ID of the post being edited (if any)
+    //Taking the Value from input
     d_post = $('#input').val();
-    
-    //Mongo Query to Insert into Collection posts
-    posts.insert({
-      post: d_post,
-      userId: Meteor.userId()
-    });
-    
-    //Clear the Input After Inserting
+    if (postId) {
+      // if we're editing an existing post, update the corresponding document in the posts collection
+      posts.update(postId, {
+        $set: {
+          post: d_post
+        }
+      });
+      // reset the editing post ID so the form will create a new post on next submit
+      Session.set('editingPostId', null);
+    } else {
+      // if we're not editing an existing post, insert a new document into the posts collection
+      posts.insert({
+        post: d_post,
+        userId: Meteor.userId()
+      });
+    }
+    //Clear the Input After Inserting/Updating
     document.getElementById('input').value = '';
   }
 });
@@ -43,11 +50,9 @@ Template.resolution.events({
   'click .edit-post-button':function(event)
   {
     let doc = posts.findOne({_id: this._id});
-    console.log("DOC POST",doc.post)
     document.getElementById('input').value = doc.post
+    Session.set('editingPostId', this._id);
   }
 
 });
-
-// @._id selects the item associated to the button in the UI
 
